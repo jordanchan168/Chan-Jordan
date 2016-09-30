df7666339de04433c62a601e0e7db40f60b931ec
jordanchan168 <jchan16@stuy.edu>
2016-09-29 22:23:44 -0400

    added files

--- app.py
+++ app.py
@@ -1,21 +1,32 @@
-from flask import Flask, render_template
-from utils import jobs
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-    return "No hablo queso"
+def home():
+    return render_template("login.html")
 
 
-@app.route("/hi")
-def hello():
-    return "Ni Hao"
+@app.route("/auth", methods=["POST"])
+def authenticate():
 
+    # DIAGNOSTICS
+    #print "\n\n\n===DIAG=== this Flask obj"
+    #print app
+    #print "\n===DIAG=== this request obj"
+    #print request
+    #print "\n===DIAG=== this request headers obj"
+    #print request.headers
+    #print "\n===DIAG=== this request method obj"
+    #print request.method
+    #print "\n===DIAG=== this request form obj"
 
-@app.route("/occupations")
-def tablify():
-    return render_template("basic.html", title="Occupations", data=jobs.dictionarify(), word=jobs.randjob())
+    if (request.form["username"]=="eublepharis" and request.form["password"]=="macularius"):
+        return render_template("results.html", result="Login successful!")
+    else:
+        return render_template("results.html", result="Login failed!")
+
 
 if __name__=="__main__":
     app.debug = True #
